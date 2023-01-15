8906a16aa3eb376884ace6d40ff85c0867e36154
Jaewook <jwyeon318@naver.com>
2023-01-16 00:23:17 +0900

    완성!

--- Number Guessing Game/main.py
+++ Number Guessing Game/main.py
@@ -1,7 +1,7 @@
 import random
 import art
-def hard_game():
-    attempts = 5
+def play_game(attempt):
+    attempts = attempt
     random_num = random.randint(1,100)
     is_game_over = False
     print(f'You have {attempts} attempts remaining to guess the number.')
@@ -26,40 +26,14 @@ def hard_game():
                     print('Too high')
                 print('''You've run out of guesses, you lose.''')
                 is_game_over = True
-def easy_game():
-    attempts = 10
-    random_num = random.randint(1, 100)
-    is_game_over = False
-    print(f'You have {attempts} attempts remaining to guess the number.')
-    while not is_game_over:
-        guessed_num = int(input('Make a guess: '))
-        if random_num == guessed_num:
-            print('You win')
-            is_game_over = True
-        else:
-            attempts = attempts - 1
-            if attempts > 0:
-                print(f'You have {attempts} attempts remaining to guess the number.')
-                if random_num > guessed_num:
-                    print('Too low')
-                elif random_num < guessed_num:
-                    print('Too high')
-                print('Guess again')
-            else:
-                if random_num > guessed_num:
-                    print('Too low')
-                elif random_num < guessed_num:
-                    print('Too high')
-                print('''You've run out of guesses, you lose.''')
-                is_game_over = True
 print(art.logo)
 print('Welcome to the Number Guessing Game!')
 print('''I'm am thinking of a number between 1 and 100.''')
 difficulty = input('''Choose a difficulty. Type 'easy' or 'hard': ''')
 if difficulty == 'easy':
-    easy_game()
+    play_game(10)
 
 elif difficulty == 'hard':
-    hard_game()
+    play_game(5)
 
 
