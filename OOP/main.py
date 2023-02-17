7f1949310d368b65dd17d17a41de325feb29ffa6
JaewookYeon <jwyeon318@naver.com>
2023-02-17 16:05:53 +0900

    update

--- OOP/main.py
+++
@@ -1,9 +0,0 @@
-from prettytable import PrettyTable
-table = PrettyTable()
-
-table.add_column("Pokemon Name",["a","b","c"])
-table.add_column("Type",["z","x","c"])
-
-table.align="l"
-print(table)
-
